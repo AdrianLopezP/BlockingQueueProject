d508c7693a25f973b231a1c00d11f565bc6fc177
DevRollins <drollins1108@gmail.com>
2024-03-13 16:00:47 -0400

    Got to an 85.

--- MyBlockingQueue.java
+++ MyBlockingQueue.java
@@ -1,3 +1,4 @@
+
 /**
  * Models a thread-safe Blocking Queue.
  *
@@ -5,7 +6,6 @@
  * @author Adrian Lopez
  * @author Devin Rollins
  */
-
 
 import java.util.*;
 import java.util.concurrent.Semaphore;
@@ -16,48 +16,55 @@ public class MyBlockingQueue<T> {
 
     LinkedList<T> blockList = new LinkedList<T>();
 
-    public MyBlockingQueue(int x){
+    public MyBlockingQueue(int x) {
         this.maxElements = x;
     }
 
-    //Takes a generic parameter (element) and adds it to the back of the queue
+    // v - release - incremet semaphore
+    // p - aquire - block until positive
+    // Takes a generic parameter (element) and adds it to the back of the queue
     public synchronized void add(T element) {
-        if (blockList.size() < maxElements){
-            blockList.add(element);
+        if (blockList.size() < maxElements) {
+            try {
+                semBoy.release(); // Grab a token from the semaphore
+                blockList.add(element); // Then add it to the
+            } catch (Exception e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Thread Interrupted");
+            }
+
         } else {
             System.out.println("Queue is full");
         }
     }
 
-
     // prints out the elements in the queue
     @Override
-    public String toString(){
+    public String toString() {
         String output = blockList.toString();
         return output;
     };
-    
-    //removes and returns the element at the front of the queue
-    public synchronized T remove(){
-            try {
-                while (blockList.size() == 0) { //While empty, wait
-                    wait();
-                } //After the while loop is broken out of, do this
-                return blockList.remove();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt(); 
-                System.err.println("Thread Interrupted");
-            }
-        return blockList.remove();
-    } 
+
+    // removes and returns the element at the front of the queue
+    public synchronized T remove() {
+        System.out.println(blockList.toString());
+        try {
+            semBoy.acquire();
+            return blockList.remove();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Thread Interrupted");
+            return null;
+        }
+    }
 
     // returns the number of elements currently stored in the queue
-    public int getNumElements(){
+    public int getNumElements() {
         return blockList.size();
     }
 
     // returns the number of empty spots in the queue
-    public int getFreeSpace(){
+    public int getFreeSpace() {
         return maxElements - blockList.size();
     }
 };
