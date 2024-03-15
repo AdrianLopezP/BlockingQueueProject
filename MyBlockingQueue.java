f038b39b7955187714131c21569f3e94a90f2f7f
DevRollins <drollins1108@gmail.com>
2024-03-15 16:31:51 -0400

    Got to an 85 fr this time

--- MyBlockingQueue.java
+++ MyBlockingQueue.java
@@ -1,4 +1,3 @@
-
 /**
  * Models a thread-safe Blocking Queue.
  *
@@ -24,18 +23,24 @@ public class MyBlockingQueue<T> {
     // p - aquire - block until positive
     // Takes a generic parameter (element) and adds it to the back of the queue
     public synchronized void add(T element) {
-        if (blockList.size() < maxElements) {
+        if (blockList.size() == maxElements){
             try {
-                semBoy.release(); // Grab a token from the semaphore
-                blockList.add(element); // Then add it to the
+                System.out.println("Queue is full");
+                semBoy.acquire();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Thread Interrupted");
+            }
+        } else {
+            try {
+                blockList.add(element);
+                semBoy.release();
+                // Then add it to the   
             } catch (Exception e) {
                 Thread.currentThread().interrupt();
                 System.err.println("Thread Interrupted");
             }
-
-        } else {
-            System.out.println("Queue is full");
-        }
+        }   
     }
 
     // prints out the elements in the queue
@@ -47,16 +52,42 @@ public class MyBlockingQueue<T> {
 
     // removes and returns the element at the front of the queue
     public synchronized T remove() {
-        System.out.println(blockList.toString());
-        try {
-            semBoy.acquire();
-            return blockList.remove();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            System.err.println("Thread Interrupted");
-            return null;
+        if (blockList.isEmpty()){
+            try {
+                System.out.println("Queue is empty");
+                semBoy.acquire();
+                return null;
+            } catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+                System.err.println("Thread Interrupted");
+                return null;
+            }
+        } else {
+            try {
+                semBoy.release();
+                return blockList.remove();      
+            }   catch (Exception e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Thread Interrupted");
+                return null;
+            }
         }
+
+            
     }
+
+        // if (!blockList.isEmpty()){
+        //     try {
+        //         semBoy.acquire();
+        //         return blockList.remove();
+                
+        //     }   catch (Exception e) {
+        //         Thread.currentThread().interrupt();
+        //         System.err.println("Thread Interrupted");
+        //         return null;
+        //     }
+        // }
+
 
     // returns the number of elements currently stored in the queue
     public int getNumElements() {
